517ac9c824449af663c055492798d77019a40943
Peritract <dan@getfutureproof.co.uk>
2022-04-17 13:18:21 +0100

    Add login route

--- api/api.js
+++ api/api.js
@@ -3,11 +3,13 @@ const express = require("express");
 const logRoutes = require("./middleware/log-routes");
 
 const userController = require("./controllers/user");
+const authController = require("./controllers/auth");
 
 const api = express();
 
 api.use(logRoutes);
 
 api.use("/users", userController);
+api.use("/auth", authController);
 
 module.exports = api;
---
+++ api/controllers/auth.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const e = require("express");
+
+const authController = Router();
+
+authController.post("/login", bodyParser.json(), async (req, res) => {
+    try {
+        const user = await User.getUserByUsername(req.body.username);
+
+        const authenticated = await bcrypt.compare(req.body.password, user.password);
+
+        if (authenticated) {
+            const token = await jwt.sign(user.details,
+                                         process.env.JWT_SECRET,
+                                         { expiresIn: 60 * 60 });
+            res.status(200).send({ success : true, token : "Bearer " + token});
+        } else {
+            throw { code : 403, message : "Unable to authenticate" };
+        }
+    } catch (err) {
+        if (!err.hasOwnProperty("code")) {
+            err = {code : 500, message : err.message }
+        }
+        res.status(err.code).json({ success : false, message : err.message })
+    }
+})
+
+module.exports = authController;
